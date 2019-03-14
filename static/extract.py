1b04eb7657c8706ae522302a8cc54d4382d6c2cf
liangxun <liangxun21@gmail.com>
2019-03-14 17:50:58 +0800

    modify static

--- static/extract.py
+++ static/extract.py
@@ -1,6 +1,11 @@
 import os
 from lxml import etree
 import json
+import logging.config
+
+log_conf = "./logging.conf"
+logging.config.fileConfig(log_conf)
+logger = logging.getLogger('report')
 
 NS_ANDROID_URI = 'http://schemas.android.com/apk/res/android' 
 NS_ANDROID = '{{{}}}'.format(NS_ANDROID_URI)
@@ -20,6 +25,8 @@ class Extractor:
                 CallerClass, CallerMehod = line.split(',')[:2]
                 api = 'L' + CallerClass + ';->' + CallerMehod
                 api = api.strip()
+                # if api in APIs:
+                    # print("Redundant api: {}".format(api))
                 APIs.add(api)
         print("build dict: contain {} sensitive APIs.".format(len(APIs)))
         return APIs
@@ -46,21 +53,27 @@ class Extractor:
             root_tpl = os.path.join(smali_path, tpl)
             for a, _, _ in os.walk(root_tpl):
                 tpl_codes.add(a)
-        all_apis = []
+        all_apis = set()
         for a, _, c in os.walk(smali_path):
             if a not in tpl_codes:    # 不统计第三方库中的api调用情况
                 for file in c:
                     single_smali_file = os.path.join(a,file)
                     # print(single_smali_file)
                     apis = self.extract_api(single_smali_file)
-                    all_apis += apis
+                    all_apis = all_apis | apis
         return all_apis
 
     def extract_api(self, smali_file):
-        apis = []
+        apis = set()
         with open(smali_file, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
+                if line.startswith('invoke'):
+                    func = line.split(' ')[-1]
+                    func = func[:func.index('(')]
+                    if func in self.sensitive_apis:
+                        # print(func)
+                        apis.add(func)
         return apis
 
     def extract(self, apk):
@@ -70,8 +83,33 @@ class Extractor:
         uses_permissions = self.parse_manifest(manifest_path)
         tpls = self.get_tpl(tpl_path)
         apis = self.analysis_smali(smali_path, tpls)
-        return uses_permissions, apis, tpls
+        return uses_permissions, list(apis), tpls
 
 
 if __name__ == '__main__':
-    
+    api_dict = '/home/thinker/Projects/Android/data/mapping_5.1.1.csv'
+
+    path_decompiled = '/media/thinker/DATA/BUPT/Codes/Data/SoftwareSecurity/decompiled/malware'
+    path_tpl = '/media/thinker/DATA/BUPT/Codes/Data/SoftwareSecurity/TPL/malware-tpl'
+    out_path = "/home/thinker/Projects/Android/data/malware_report"
+    E = Extractor(path_decompiled, path_tpl, api_dict)
+    # apk = '44cf724e3c80c2eea38ba1020bc67f52.apk'
+    apks_path = "/media/thinker/DATA/BUPT/Codes/Data/SoftwareSecurity/malware_apks"
+    cnt = 1
+    for apk in os.listdir(apks_path):
+        if os.path.exists(os.path.join(out_path, apk)):
+            logger.info("{}, {} already exists".format(cnt, apk))
+        else:
+            try:
+                logger.info("{}, {}".format(cnt, apk))
+                a, b, c = E.extract(apk)
+                report = {"permission": a, "sensitive_api": b, "tpl": c}
+                # print(json.dumps(report, indent=4, sort_keys=True))
+                report_file = os.path.join(out_path, apk)
+                with open(report_file, 'w') as f:
+                    json.dump(report, f, indent=4, sort_keys=True)
+            except Exception as e:
+                logger.error("{}, error \n{}".format(apk,e))
+                pass
+        cnt += 1
+
